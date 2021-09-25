13f73c30d5563e4d91bfdccc3d12a1240d726a7d
Matt <aboutmattlaw@gmail.com>
2021-09-25 18:07:38 -0400

    removed boilerplate, quick placeholder content in App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>This is going to be a small app to help with knowing when I need to move the car.</h1> 
+      <h2><a href="https://www1.nyc.gov/html/dot/downloads/pdf/asp-calendar-2021.pdf">Alternate Side Parking Calendar</a></h2>
+     </div>
+  );
+}
+
+export default App;
